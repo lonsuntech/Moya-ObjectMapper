08f25876f3ee234fed5708876d7326d308675340
Liu PeiPei <lsanotes@live.com>
2019-06-13 15:32:35 +0800

    release 3.1
    add public modifier to func in extension

--- Source/RxSwift/Single+ObjectMapper.swift
+++ Source/RxSwift/Single+ObjectMapper.swift
@@ -16,7 +16,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an object
     /// which implements the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
+    public func mapObject<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
         return flatMap { response -> Single<T> in
                     return Single.just(try response.mapObject(type, context: context))
                 }
@@ -25,7 +25,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an array of objects
     /// which implement the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
+    public func mapArray<T: BaseMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
         return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, context: context))
                 }
@@ -34,7 +34,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an object
     /// which implements the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-  func mapObject<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<T> {
+    public func mapObject<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<T> {
         return flatMap { response -> Single<T> in
           return Single.just(try response.mapObject(type, atKeyPath: keyPath, context: context))
                 }
@@ -43,7 +43,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an array of objects
     /// which implement the Mappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapArray<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<[T]> {
+    public func mapArray<T: BaseMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<[T]> {
         return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, atKeyPath: keyPath, context: context))
                 }
@@ -57,7 +57,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an object
     /// which implements the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
+    public func mapObject<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<T> {
         return flatMap { response -> Single<T> in
                     return Single.just(try response.mapObject(type, context: context))
                 }
@@ -66,7 +66,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an array of objects
     /// which implement the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
+    public func mapArray<T: ImmutableMappable>(_ type: T.Type, context: MapContext? = nil) -> Single<[T]> {
         return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, context: context))
                 }
@@ -75,7 +75,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an object
     /// which implements the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapObject<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<T> {
+    public func mapObject<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<T> {
         return flatMap { response -> Single<T> in
                     return Single.just(try response.mapObject(type, atKeyPath: keyPath, context: context))
                 }
@@ -84,7 +84,7 @@ public extension PrimitiveSequence where Trait == SingleTrait, Element == Respon
     /// Maps data received from the signal into an array of objects
     /// which implement the ImmutableMappable protocol and returns the result back
     /// If the conversion fails, the signal errors.
-    func mapArray<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<[T]> {
+    public func mapArray<T: ImmutableMappable>(_ type: T.Type, atKeyPath keyPath: String, context: MapContext? = nil) -> Single<[T]> {
         return flatMap { response -> Single<[T]> in
                     return Single.just(try response.mapArray(type, atKeyPath: keyPath, context: context))
                 }
